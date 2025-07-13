3953ca291396188efe94e48e1b91ba6bc542773a
ivona <ik87056@student.uwb.edu.pl>
2025-07-13 21:01:40 +0200

    update

--- src/main/web/WEB-INF/classes/Ss01.java
+++ src/main/web/WEB-INF/classes/Ss01.java
@@ -1,28 +1,17 @@
 import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/Sso1")
-public class Ss01 extends HttpServlet {
+public class Ss01 extends HttpServlet{
+    private static final long serialVersionUID = 1L;
 
     public Ss01() {
-        System.out.println("Ss01:constructor");
-    }
-
-    public void init(ServletConfig config) throws ServletException {
-        System.out.println("Ss01:init");
-    }
-
-    public void destroy() {
-        System.out.println("Ss01:destroy");
-    }
-
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Ss01:service");
+        super();
+        System.out.println("Ss01");
     }
 }
