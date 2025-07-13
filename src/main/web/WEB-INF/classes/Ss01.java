916bfe906a7efbb1d6ab41e226a4c0e758dee366
ivona <ik87056@student.uwb.edu.pl>
2025-07-13 21:09:02 +0200

    update

--- src/main/web/WEB-INF/classes/Ss01.java
+++ src/main/web/WEB-INF/classes/Ss01.java
@@ -7,11 +7,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class Ss01 extends HttpServlet{
+public class Ss01 extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     public Ss01() {
         super();
         System.out.println("Ss01");
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init");
+    }
+
+    public void destroy() {
+        System.out.println("destroy");
+    }
+
+    protected void service(HttpServletRequest request, HttpServletResponce responce) throws ServletException, IOException {
+        System.out.println("service");
     }
 }
